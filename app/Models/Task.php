51abca77a7502929b78ea3ff97243265ed6f6cb0
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-10-01 21:52:11 +0600

    Relation with Taks to user & User to Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'user_id'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Task;
+
+
 
 class User extends Authenticatable
 {
@@ -46,5 +49,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function tasks(){
+        return $this->hasMany(Task::class);
     }
 }
